968baf09a69496d63ac5f4eb29034353692e2694
IonRC <92815958+IonRC@users.noreply.github.com>
2022-03-15 11:31:31 +0200

    init  section us

---
+++ src/components/section_ion/section-us.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class SectionUs extends Component {
+    render() {
+        return (
+            <div>
+                sf
+            </div>
+        );
+    }
+}
+
+export default SectionUs;
