afeee36e7761ff5816af3cf86d1b91b49819c22e
IonRC <92815958+IonRC@users.noreply.github.com>
2022-03-15 11:04:26 +0200

    init  project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+     <div>
+         your components here
+     </div>
+  );
+};
+
+export default App;
