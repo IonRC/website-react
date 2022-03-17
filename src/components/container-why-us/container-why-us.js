60cee0473b69954a3f802e6ce8420f761a68ea9b
IonRC <92815958+IonRC@users.noreply.github.com>
2022-03-17 11:44:22 +0200

    init  project

---
+++ src/components/container-why-us/container-why-us.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class ContainerWhyUs extends Component {
+    render() {
+        return (
+            <div>
+                Ion's section here
+            </div>
+        );
+    }
+}
+
+export default ContainerWhyUs;
