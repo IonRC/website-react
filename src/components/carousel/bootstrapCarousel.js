397de6bb69c638a84b61f44cc0a95c1b903e97dc
Cristi2002 <cristianistrate1234@gmail.com>
2022-03-17 22:06:46 +0300

    Carousel Cristi

--- src/components/carousel/bootstrapCarousel.js
+++ src/components/carousel/bootstrapCarousel.js
@@ -3,48 +3,46 @@ import {Carousel} from "react-bootstrap";
 
  function BootstrapCarousel(){
     return(
-        <div>
-            <Carousel>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="https://www.advamed.org/wp-content/uploads/2021/05/committee-meeting-board-room.jpg"
-                        alt="First slide"
-                    />
-                    <Carousel.Caption>
-                        <h3>24/7 Tech & Business Support</h3>
-                        <p>We ensure lifetime support of all applications we've built.
-                            Our support department is a team of professionals who will assist you 24/7</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="https://www.advamed.org/wp-content/uploads/2021/05/committee-meeting-board-room.jpg"
-                        alt="Second slide"
-                    />
+     <Carousel fade>
+         <Carousel.Item>
+             <img
+                 className="d-block w-100"
+                 src="https://www.advamed.org/wp-content/uploads/2021/05/committee-meeting-board-room.jpg"
+                 alt="First slide"
+             />
+             <Carousel.Caption>
+                 <h3>24/7 Tech & Business Support</h3>
+                 <p>We ensure lifetime support of all applications we've built.
+                     Our support department is a team of professionals who will assist you 24/7</p>
+             </Carousel.Caption>
+         </Carousel.Item>
+         <Carousel.Item>
+             <img
+                 className="d-block w-100"
+                 src="https://www.advamed.org/wp-content/uploads/2021/05/committee-meeting-board-room.jpg"
+                 alt="Second slide"
+             />
 
-                    <Carousel.Caption>
-                        <h3>Award-Winning Software Development</h3>
-                        <p>We build complex web , desktop and mobile applications.
-                            With us you get quality software and perfect service every time.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="https://www.advamed.org/wp-content/uploads/2021/05/committee-meeting-board-room.jpg"
-                        alt="Third slide"
-                    />
+             <Carousel.Caption>
+                 <h3>Award-Winning Software Development</h3>
+                 <p>We build complex web , desktop and mobile applications.
+                     With us you get quality software and perfect service every time.</p>
+             </Carousel.Caption>
+         </Carousel.Item>
+         <Carousel.Item>
+             <img
+                 className="d-block w-100"
+                 src="https://www.advamed.org/wp-content/uploads/2021/05/committee-meeting-board-room.jpg"
+                 alt="Third slide"
+             />
 
-                    <Carousel.Caption>
-                        <h3>The best IT solutions for your business</h3>
-                        <p>We provide the wide range of high quality IT services and
-                            best practices solutions to our costumers making their business better.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
-        </div>
+             <Carousel.Caption>
+                 <h3>The best IT solutions for your business</h3>
+                 <p>We provide the wide range of high quality IT services and
+                     best practices solutions to our costumers making their business better.</p>
+             </Carousel.Caption>
+         </Carousel.Item>
+     </Carousel>
     )
 }
 
