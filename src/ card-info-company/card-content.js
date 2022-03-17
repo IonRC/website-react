3b3f35c7b9cecd191d10e27a5726f83010bc5fec
Kalepson <kalepson@gmail.com>
2022-03-17 13:04:20 +0200

    Am creat card-info-company in interior am creat structura la card cu un object cu toate datele din card.js

---
+++ src/ card-info-company/card-content.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const [contentOfTheTitle, btnContent, footerContent, srcImg] = [
+    {
+        title: "Award-Winning Software Company",
+        content: "We create diverse, complex, web and mobile solutions for any business need. With us you get quality software and perfect service every time."
+    },
+    {
+        title: "More about us",
+    },
+    {
+        title: "Some of our awards:",
+    },
+    {
+        img: "https://silicon.createx.studio/assets/img/landing/software-company/about-img.jpg",
+        icon: "https://silicon.createx.studio/assets/img/landing/software-company/awards.svg"
+    }
+
+]
+
+export {contentOfTheTitle, btnContent, footerContent, srcImg}
---
+++ src/ card-info-company/card.js
@@ -0,0 +1,36 @@
+import card from "./card.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, {Component} from 'react';
+import {Col} from "react-bootstrap";
+import {contentOfTheTitle, btnContent, footerContent, srcImg} from "./card-content"
+
+
+class CardComponentStructure extends Component {
+    render() {
+        return (
+            <Col>
+                <section className={"sectionContainer"}>
+                    <div className={"container"}>
+                        <div className={"card-contant col col-md-5 text-center text-md-start"}>
+                            <h1 className={"nameTheContent"}>{contentOfTheTitle.title}</h1>
+                            <p className={"theContent"}> {contentOfTheTitle.content}</p>
+                            <a className="btn btn-primary shadow-primary btn-lg">
+                                {btnContent.title}
+                            </a>
+                            <div className={""}>
+                                <h2 className={"some"}>{footerContent.title}</h2>
+                                <img alt={"icon"} className={"imgAssets"} src={srcImg.icon}/>
+                            </div>
+                        </div>
+                        <div className={""}>
+                            <img alt={"imgCompany"} className={"imgCard img-fluid"} src={srcImg.img}/>
+                        </div>
+                    </div>
+                </section>
+            </Col>
+
+        );
+    }
+}
+
+export default CardComponentStructure;
